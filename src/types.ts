a8db93a871765d0b5d1bbe1995ec10e6d0b23f2e
James Grubb <james@jamesgrubb.co.uk>
2024-08-27 22:41:33 +0100

    adding type autopopulate

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,8 @@ export interface GridHandler {
   name: 'UPDATE_GRID'
   handler: (options: { cellCount: number, padding: number }) => void
 }
+
+export interface AutoPopulateHandler {
+  name: 'AUTO_POPULATE'
+  handler: (options: { autoPopulate: boolean }) => void
+}
